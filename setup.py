9aa8e9d451038fb7372aebe9682ad1cf1f6c9a19
alchemyEngine <robertp@norbital.com>
2021-08-22 13:52:28 +0200

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding="utf-8") as fh:
 
 setuptools.setup(
     name="MoonrakerPy",
-    version="0.0.1",
+    version="0.0.2",
     author="Robert Pazdzior",
     author_email="robertp@norbital.com",
     description="A Python package for interface with the Moonraker API.",
